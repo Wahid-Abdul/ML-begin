60a4264d95adc5b7f30420a980b31cc6c03c3f72
Wahid-Abdul <abdu14101.cs@rmkec.ac.in>
2018-08-19 22:16:18 +0530

    day1 end

--- Code/day1.py
+++ Code/day1.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 dataset = pd.read_csv('./../datasets/Data.csv')
@@ -23,5 +25,27 @@ labelencoder_X = LabelEncoder()
 X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0 ])
 X[ : , 3] = labelencoder_X.fit_transform(X[ : , 3 ])
 
+
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y =  labelencoder_Y.fit_transform(Y)
+
+# Splitting the datasets into training sets and Test sets
+X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
+
+print("\nwahid\n")
+
 print()
-print(X)
+print(X_test)
+print()
+print(X_train)
+
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+
+print()
+print(X_test)
+print()
+print(X_train)
